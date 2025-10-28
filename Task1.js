4e2cedcb70a46b37f91d279c8c1a45b59d89490f
Yael-Salem <wael231005@gmail.com>
2025-10-28 15:28:08 +0200

    Add files via upload

--- Task1.js
+++ Task1.js
@@ -7,12 +7,12 @@ const num = 7;
 
 // Saving the boolean result of the division and casting it to a number
 let divisionResult =
-  Number(num % 2 == 0 || num % 3 == 0 || num % 5 == 0) +
+  Number(num % 2 === 0 || num % 3 === 0 || num % 5 === 0) +
   Number(
-    (num % 2 == 0 && num % 3 == 0) ||
-      (num % 3 == 0 && num % 5 == 0) ||
-      (num % 2 == 0 && num % 5 == 0)
+    (num % 2 === 0 && num % 3 === 0) ||
+      (num % 3 === 0 && num % 5 === 0) ||
+      (num % 2 === 0 && num % 5 === 0)
   ) +
-  Number(num % 2 == 0 && num % 3 == 0 && num % 5 == 0);
+  Number(num % 2 === 0 && num % 3 === 0 && num % 5 === 0);
 
 console.log(divisionResult);
--- Task2.js
+++ Task2.js
@@ -7,7 +7,7 @@ let isPrime = true;
 
 for (let i = 2; i < 237; i++) {
   // Checkng if there are any numbers (besides 1 and the value of i) that i can divide by
-  for (let j = 2; j < i; j++) if (i % j == 0) isPrime = false;
+  for (let j = 2; j < i; j++) if (i % j === 0) isPrime = false;
 
   if (isPrime) console.log(i);
 
